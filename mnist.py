68fe6377fbc2b40e3c4b3b591007607ceec4eb15
D.J. Zou <bopy2008@163.com>
2018-06-11 15:36:31 +0800

    添加步骤讲解注释

--- mnist.py
+++ mnist.py
@@ -1,16 +1,18 @@
+# remote debug
+# import ptvsd
+# ptvsd.settrace(None, ('0.0.0.0', 18110))
+# ptvsd.wait_for_attach()
+
+#首先导入一些用到的库。
 import numpy as np
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 from matplotlib import pyplot as plt
 
-
-import ptvsd
-ptvsd.settrace(None, ('0.0.0.0', 18110))
-
-ptvsd.wait_for_attach()
 #%matplotlib inline
-
 tf.logging.set_verbosity(tf.logging.INFO)
+
+#先来看看数据长什么样子
 
 mnist = input_data.read_data_sets("./")
 
@@ -23,6 +25,11 @@ print(mnist.validation.labels.shape)
 print(mnist.test.images.shape)
 print(mnist.test.labels.shape)
 
+
+#可以看到images里面有数量不等的图片，每张图片是28x28长度的一个一维向量， 所以用的时候需要先给它还原成28x28的二维图片。
+# labels中则是图片对应的数字的值。
+
+
 # plt.figure(figsize=(8,8))
 
 # for idx in range(16):
@@ -30,11 +37,20 @@ print(mnist.test.labels.shape)
 #     plt.axis('off')
 #     plt.title('[{}]'.format(mnist.train.labels[idx]))
 #     plt.imshow(mnist.train.images[idx].reshape((28,28)))
+
+#
+#接下来，定义用于训练的网络
+# 首先定义网络的输入。
+# 这里我们直接使用上面的数据作为输入，所以定义两个placeholder分别用于图像和lable数据，另外，定义一个float类型的变量用于设置学习率。
+# 为了让网络更高效的运行，多个数据会被组织成一个batch送入网络，两个placeholder的第一个维度就是batchsize，因为我们这里还没有确定batchsize，所以第一个维度留空。
 	
+# label 就是ground truth
 x = tf.placeholder("float", [None, 784])
 y = tf.placeholder("int64", [None])
 learning_rate = tf.placeholder("float")
 
+#
+#两层网络结构图的构建
 
 def initialize(shape, stddev=0.1):
   return tf.truncated_normal(shape, stddev=0.1)
@@ -46,6 +62,7 @@ b_1 = tf.Variable(initialize([L1_units_count]))
 logits_1 = tf.matmul(x, W_1) + b_1
 output_1 = tf.nn.relu(logits_1)
 
+#第二层就是输出层，不需要激活
 L2_units_count = 10 
 W_2 = tf.Variable(initialize([L1_units_count, L2_units_count], stddev=0.063))
 b_2 = tf.Variable(initialize([L2_units_count]))
@@ -53,20 +70,35 @@ logits_2 = tf.matmul(output_1, W_2) + b_2
 
 logits = logits_2
 
+#
+#接下来定义loss和用于优化网络的优化器。
+# loss计算使用了sparse_softmax_cross_entropy_with_logits, 这样做的好处是labels可以不用手动做one_hot省了一些麻烦。
+# 这里使用了sgd优化器，学习率为可以根据需要设定。
+#tf.reduce_mean 求平均值
+
 cross_entropy_loss = tf.reduce_mean(
     tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=y))
 
 optimizer = tf.train.GradientDescentOptimizer(
     learning_rate=learning_rate).minimize(cross_entropy_loss)
+
+# 需要注意的是，上面的网络，最后输出的是未经softmax的原始logits，而不是概率分布， 要想看到概率分布，还需要做一下softmax。
+
+# 将输出的结果与正确结果进行对比，即可得到我们的网络输出结果的准确率。
 	
 pred = tf.nn.softmax(logits)
 correct_pred = tf.equal(tf.argmax(pred, 1), y)
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
+# saver用于保存或恢复训练的模型。
+
 batch_size = 32
 trainig_step = 1000
 
 saver = tf.train.Saver()
+
+
+# 以上定义的所有操作，均为计算图，也就是仅仅是定义了网络的结构，实际需要运行的话，还需要创建一个session，并将数据填入网络中。
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
